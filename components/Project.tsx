c6b3ddf256dfff1b9ac66f197b19f904e8bd9a33
Akadeha420 <aungkhamnaung2003@gmail.com>
2025-03-16 22:03:17 -0500

    quick project transition

--- components/Project.tsx
+++ components/Project.tsx
@@ -86,7 +86,7 @@ const Project = () => {
             initial={{ opacity: 0, x: 50 }}
             animate={{ opacity: 1, x: 0 }}
             exit={{ opacity: 0, x: -50 }}
-            transition={{ duration: 0.5 }}
+            transition={{ duration: 0.25 }}
           >
             <ProjectItem
               {...projects[index]}
